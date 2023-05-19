48a5f3611b56c98f943856b5310be64625d3ead1
nurlancreus <nurlancreus007@gmail.com>
2023-05-19 13:50:54 +0400

    accordion changes

--- js/script.js
+++ js/script.js
@@ -22,38 +22,38 @@
 const newsTab = document.querySelector(".news-tab");
 
 // Get all the tab buttons
-const tabButtons = newsTab.querySelectorAll('.nav-link');
+const tabButtons = newsTab.querySelectorAll(".nav-link");
 
 // Get all the tab panels
-const tabPanels = document.querySelectorAll('.tab-panel');
+const tabPanels = document.querySelectorAll(".tab-panel");
 
 // Function to show the selected tab panel and hide the others
 function showTab(tabId) {
   // Hide all tab panels
-  tabPanels.forEach(panel => {
-    panel.style.display = 'none';
+  tabPanels.forEach((panel) => {
+    panel.style.display = "none";
   });
 
   // Show the selected tab panel
   const selectedPanel = document.getElementById(tabId);
   if (selectedPanel) {
-    selectedPanel.style.display = 'block';
+    selectedPanel.style.display = "block";
   }
 }
 
 // Add click event listeners to the tab buttons
-tabButtons.forEach(button => {
-  button.addEventListener('click', function() {
+tabButtons.forEach((button) => {
+  button.addEventListener("click", function () {
     // Remove the "active" class from all tab buttons
-    tabButtons.forEach(btn => {
-      btn.classList.remove('active');
+    tabButtons.forEach((btn) => {
+      btn.classList.remove("active");
     });
 
     // Add the "active" class to the clicked tab button
-    this.classList.add('active');
+    this.classList.add("active");
 
     // Get the target tab panel ID from the aria-controls attribute
-    const targetTab = this.getAttribute('aria-controls');
+    const targetTab = this.getAttribute("aria-controls");
 
     // Show the corresponding tab panel
     showTab(targetTab);
@@ -63,8 +63,8 @@ tabButtons.forEach(button => {
 // Initially show the first tab panel and add "active" class to the first tab button
 if (tabButtons.length > 0) {
   const firstButton = tabButtons[0];
-  const firstTab = firstButton.getAttribute('aria-controls');
+  const firstTab = firstButton.getAttribute("aria-controls");
   showTab(firstTab);
-  console.log(tabButtons)
-  firstButton.classList.add('active');
+  console.log(tabButtons);
+  firstButton.classList.add("active");
 }
